9237f312e0f7ef60bc09f5ef9a9e25cc33a0168e
Rajeshkuar Sivapraksam <rk.sivaprakasam@outlook.com>
2021-03-18 14:08:47 +0530

    Basic Skelton Structure

---
+++ GitPR/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace GitPR
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
